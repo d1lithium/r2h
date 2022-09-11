e6b93018cb4a39321b2c0717c5ce08e85d149ba5
Muhammad Moin Raza Khan <muhammadmoin74@gmail.com>
2022-09-11 19:08:49 +0500

    changed a flag in data classes

--- app/src/main/java/com/cyphersol/r2h/data/local/PreLogin.kt
+++ app/src/main/java/com/cyphersol/r2h/data/local/PreLogin.kt
@@ -1,7 +1,7 @@
 package com.cyphersol.r2h.data.local
 
 data class PreLogin(
-    val alreadyLoggedIn: Boolean = false,
+    val firstLogin: Boolean = false,
     val cnicVerified: Boolean = false,
     val cnicUploaded: Boolean = false,
     val networkAvailable: Boolean = false
