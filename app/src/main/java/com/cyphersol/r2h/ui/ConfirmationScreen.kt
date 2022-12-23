6814db3be4dd51388a8ac407bf59c431f9141feb
Muhammad Moin <muhammadmoin74@gmail.com>
2022-12-23 16:04:10 +0500

    formatted the code

--- app/src/main/java/com/cyphersol/r2h/ui/ConfirmationScreen.kt
+++ app/src/main/java/com/cyphersol/r2h/ui/ConfirmationScreen.kt
@@ -4,33 +4,48 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.cyphersol.r2h.ui.theme.R2hTheme
 
-class ConfirmationScreen: ComponentActivity() {
+class ConfirmationScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        setContent { 
-            R2hTheme{
-             ChoiceScreen(isSuccess = true)   
+        setContent {
+            R2hTheme {
+                ChoiceScreen(isSuccess = true)
             }
         }
     }
+
     @Composable
-    fun ChoiceScreen(isSuccess: Boolean){
-        if(isSuccess){
+    fun ChoiceScreen(isSuccess: Boolean) {
+        if (isSuccess) {
             SuccessScreen()
-        }
-        else{
+        } else {
             FailureScreen()
         }
     }
+
     @Composable
-    fun SuccessScreen(){
+    fun SuccessScreen() {
+        Column(verticalArrangement = Arrangement.Center) {
+
+        }
+    }
+
+    @Composable
+    fun FailureScreen() {
 
     }
+
+    @Preview(showBackground = true)
     @Composable
-    fun FailureScreen(){
-        
+    fun Preview3() {
+        R2hTheme {
+            ChoiceScreen(isSuccess = true)
+        }
     }
 }
